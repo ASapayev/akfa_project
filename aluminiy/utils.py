b91b73a71f7c6ba88ebfd2ebebb6a8058ba5d9a2
MuzaffarTursunov96 <muzaffar.tursunov102196@gmail.com>
2023-03-28 15:01:53 -0800

    sfsdfds

--- aluminiy/utils.py
+++ aluminiy/utils.py
@@ -15,6 +15,5 @@ def do_exist(artikules):
 def create_folder(parent_dir,directory):
     path =os.path.join(parent_dir,directory)
     if not os.path.isdir(path):
-        path = os.path.join(parent_dir, directory)
         os.mkdir(path)
             
