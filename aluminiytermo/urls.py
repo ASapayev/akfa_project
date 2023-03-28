52ab883ded1d00b6b51621f9da835227337d2aab
MuzaffarTursunov96 <muzaffar.tursunov102196@gmail.com>
2023-03-28 12:38:47 -0800

    hhh

--- aluminiytermo/urls.py
+++ aluminiytermo/urls.py
@@ -2,8 +2,7 @@ from django.urls import path,re_path
 from . import views
 
 urlpatterns = [
-#  path('')
-path('aluminiy',views.artikul_and_companent,name='aluminiy'),
+
 path('upload-product',views.upload_product,name='upload_product'),
 path('aluminiy-files',views.aluminiy_files,name='aluminiy_files'),
 path('product-base',views.alu_product_base,name='alu_product_base'),
