fb02a9d8b16155400e4564805b541162ea57fb59
MuzaffarTursunov96 <muzaffar.tursunov102196@gmail.com>
2023-06-05 21:27:34 +0500

    razlovka save

--- aluminiy/views.py
+++ aluminiy/views.py
@@ -2618,6 +2618,30 @@ def product_add_second(request,id):
       return redirect('upload_product')
                   
      
+def razlovka_save(request):
+      df_new = pd.read_excel(f'{MEDIA_ROOT}/obichniy.xlsx')
+
+      for key,razlov in df_new.iterrows():
+            if not RazlovkaObichniy.objects.filter(sap_code7=razlov['SAP код 7'],kratkiy7=razlov['U-Упаковка + Готовая Продукция 7']).exists():
+                  RazlovkaObichniy(
+                        esap_code =razlov['SAP код E'],
+                        ekratkiy =razlov['Экструзия холодная резка'],
+                        zsap_code =razlov['SAP код Z'],
+                        zkratkiy =razlov['Печь старения'],
+                        psap_code =razlov['SAP код P'],
+                        pkratkiy =razlov['Покраска автомат'],
+                        ssap_code =razlov['SAP код S'],
+                        skratkiy =razlov['Сублимация'],
+                        asap_code =razlov['SAP код A'],
+                        akratkiy =razlov['Анодировка'],
+                        lsap_code =razlov['SAP код L'],
+                        lkratkiy =razlov['Ламинация'],
+                        nsap_code =razlov['SAP код N'],
+                        nkratkiy =razlov['Наклейка'],
+                        sap_code7 =razlov['SAP код 7'],
+                        kratkiy7 =razlov['U-Упаковка + Готовая Продукция']
+                  ).save()
+      return JsonResponse({'a':'b'})
 @csrf_exempt
 def add_char_utils_two(request):
       data = request.POST.get('data',None)
