dfefba0630bbbfe5a43cca31d90209de890a6ad4
MuzaffarTursunov96 <muzaffar.tursunov102196@gmail.com>
2023-07-15 10:46:01 +0500

    sdaf

--- accounts/context_processor.py
+++ accounts/context_processor.py
@@ -15,10 +15,12 @@ ROLE_CHOICES ={
 }
 
 def get_user_role(request):
-    user_role =request.user.role
-    if user_role ==None:
-        user_role='nan'
+    if request.user.is_authenticated:
+        user_role =request.user.role
+        if user_role ==None:
+            user_role='nan'
+        else:
+            user_role =ROLE_CHOICES[user_role]
     else:
-        user_role =ROLE_CHOICES[user_role]
-    
+        user_role ='nan'
     return {'user_role':str(user_role)}
