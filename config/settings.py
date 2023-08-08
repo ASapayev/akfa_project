f2d88b55ba869e0dfdf648877fb991e04a0a4126
MuzaffarTursunov96 <muzaffar.tursunov102196@gmail.com>
2023-08-08 16:24:23 +0500

    sdf

--- config/settings.py
+++ config/settings.py
@@ -95,15 +95,6 @@ WSGI_APPLICATION = 'config.wsgi.application'
 
 DATABASES = {
     'default': {
-        # 'ENGINE': 'django.db.backends.sqlite3',
-        # 'NAME': BASE_DIR / 'db.sqlite3',
-        # 'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        # 'NAME': 'akfa', 
-        # 'USER': 'postgres',
-        # 'PASSWORD': 'postgres',
-        # 'HOST': '127.0.0.1', 
-        # 'PORT': '5432',
-
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
         'NAME': config('NAME'),
         'USER': config('USER'),
