c31c4bb793d7fc4f01031613f0013c6b1fa96311
MuzaffarTursunov96 <muzaffar.tursunov102196@gmail.com>
2023-08-10 10:40:42 +0500

    length of profile corected

--- aluminiytermo/views.py
+++ aluminiytermo/views.py
@@ -5174,6 +5174,7 @@ def product_add_second_org(request,id):
                   df_char_title['Price'][key] = float(price.price.replace(',','.')) * float(df_char_title['Общий вес за штуку'][key].replace(',','.'))  * float(exchange_value.valute.replace(',','.'))
             elif row['ch_combination'].lower() ==str("Без термомоста").lower():
                   if LengthOfProfile.objects.filter(artikul=row['ch_article']).exists():
+                        length_of_profile = LengthOfProfile.objects.filter(artikul=row['ch_article'],length=row['Длина'])[:1].get()
                         df_char_title['Общий вес за штуку'][key] =float(length_of_profile.ves_za_metr.replace(',','.')) * float(row['Длина'].replace(',','.'))
                         df_char_title['Удельный вес за метр'][key] = length_of_profile.ves_za_metr
                         price = Price.objects.filter(tip_pokritiya = row['Тип покрытия'],tip=row['ch_combination'])[:1].get()
