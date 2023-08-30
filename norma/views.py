6d60d92e6ac369c8683f5286906f25ff2eca7153
MuzaffarTursunov96 <muzaffar.tursunov102196@gmail.com>
2023-08-30 17:35:15 +0500

    d

--- norma/views.py
+++ norma/views.py
@@ -223,8 +223,8 @@ def download_zip_file(request):
         file_pathh =ast.literal_eval(file_path)
         file_path =file_pathh[0]
     if file_path:
-        filename_n = os.path.basename(file_path)
-        filename = filename_n.replace(' ','-')
+        filename = os.path.basename(file_path)
+        # filename = filename_n.replace(' ','-')
         print(filename,'*'*70)
 
         fl = open(file_path,'rb')
