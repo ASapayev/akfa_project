a089e61478830f0f8faf71539368df3fbded5480
MuzaffarTursunov96 <muzaffar.tursunov102196@gmail.com>
2023-09-07 16:15:45 +0500

    corected

--- main/views.py
+++ main/views.py
@@ -192,8 +192,8 @@ def get_ozmka(ozmk,zavod1101,zavod1201):
     return [path1201,''],[df_termo_1201,df_obichniy_1201,df_yoqlari_1201]
   
   if zavod1101:
-    termo_razlovka1101 =[ raz[4:10] + raz[12:16] + raz[18:20] for raz in termo_razlovka]
-    obichniy_razlovka1101 =[ raz[1:9] + raz[15:17] for raz in obichniy_razlovka]
+    termo_razlovka1101 =[ ['',''] + raz[4:10] + raz[12:16] + raz[18:20] for raz in termo_razlovka]
+    obichniy_razlovka1101 =[ raz[1:9] + ['','','',''] + raz[15:17] for raz in obichniy_razlovka]
     counter = 0
     obichniy_razlovka1101org = []
     for obichniy in obichniy_razlovka1101:
@@ -204,8 +204,8 @@ def get_ozmka(ozmk,zavod1101,zavod1201):
         obichniy_razlovka1101org.append(obichniy)
       counter += 1
 
-    df_termo_1101 = pd.DataFrame(termo_razlovka1101,columns=['SAP код Z','Печь старения','SAP код P','Покраска автомат','SAP код S','Сублимация','SAP код N','Наклейка','SAP код K','K-Комбинирования','SAP код 7','U-Упаковка + Готовая Продукция'])#,'CREATED DATE','UPDATED DATE'
-    df_obichniy_1101 = pd.DataFrame(obichniy_razlovka1101org,columns=['SAP код E','Экструзия холодная резка','SAP код Z','Печь старения','SAP код P','Покраска автомат','SAP код S','Сублимация','SAP код 7','U-Упаковка + Готовая Продукция'])#,'CREATED DATE','UPDATED DATE'
+    df_termo_1101 = pd.DataFrame(termo_razlovka1101,columns=['SAP код E','Экструзия холодная резка','SAP код Z','Печь старения','SAP код P','Покраска автомат','SAP код S','Сублимация','SAP код N','Наклейка','SAP код K','K-Комбинирования','SAP код 7','U-Упаковка + Готовая Продукция'])#,'CREATED DATE','UPDATED DATE'
+    df_obichniy_1101 = pd.DataFrame(obichniy_razlovka1101org,columns=['SAP код E','Экструзия холодная резка','SAP код Z','Печь старения','SAP код P','Покраска автомат','SAP код S','Сублимация','SAP код N','Наклейка','SAP код K','K-Комбинирования','SAP код 7','U-Упаковка + Готовая Продукция'])#,'CREATED DATE','UPDATED DATE'
     df_yoqlari_1101 = pd.DataFrame({'SAP CODE':sap_code_yoqlari})
     now =datetime.now()
     minut =now.strftime('%M-%S')
