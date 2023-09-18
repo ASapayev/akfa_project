66886c532efecd4fa9672ab516bad5946b3df8d1
MuzaffarTursunov96 <muzaffar.tursunov102196@gmail.com>
2023-09-18 11:48:17 +0500

    bv

--- chat/consumers.py
+++ chat/consumers.py
@@ -15,7 +15,7 @@ class ChatConsumer(WebsocketConsumer):
        
     
     def receive(self, text_data):
-        print(text_data)
+        
         text_data_json = json.loads(text_data)
         message = text_data_json['message'] 
 
