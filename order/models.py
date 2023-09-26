eb4b248ef83445033ea2d89c90af26fcba231b0f
MuzaffarTursunov96 <muzaffar.tursunov102196@gmail.com>
2023-09-26 15:43:02 +0500

    model created for pvc

--- order/models.py
+++ order/models.py
@@ -42,3 +42,36 @@ class Order(models.Model):
     order_type = models.SmallIntegerField(choices=ORDER_TYPE_CHOICES,default=1)
     created_at =models.DateTimeField(auto_now_add=True)
     updated_at =models.DateTimeField(auto_now=True)
+
+STATUS_CHOICES_PVC =(
+     (1,'ON HOLD'),
+    (2,'ON PROCESS'),
+    (3,'CANCEL'),
+    (4,'DONE')
+)
+
+WORK_TYPE_CHOICES_PVC =(
+    (1,'ON HOLD'),
+    (2,'SAP CODE CREATING'),
+    (3,'SAP CODE CREATING LACKS'),
+    (4,'TEXT CREATING'),
+    (5,'TEXT CREATING LACKS'),
+    (10,'DONE')
+)
+ORDER_TYPE_CHOICES_PVC =(
+    (1,'ЛАМ'),
+    (2,'БЕЗ ЛАМ')
+)
+
+class OrderPVX(models.Model):
+    title = models.CharField(max_length=150)
+    status = models.SmallIntegerField(choices=STATUS_CHOICES_PVC,default=1)
+    work_type =models.SmallIntegerField(choices=WORK_TYPE_CHOICES_PVC,default=1)
+    owner = models.ForeignKey(User,on_delete=models.CASCADE)
+    current_worker = models.ForeignKey(User,on_delete=models.CASCADE,blank=True,null=True,related_name='on_time_worker_pvc')
+    pvc_worker = models.ForeignKey(User,models.CASCADE,blank=True,null=True,related_name='pvc_work')
+    pvc_wrongs = models.ForeignKey(User,on_delete=models.CASCADE,blank=True,null=True,related_name='pvc_work_wrong')
+    paths = models.JSONField(null=True,blank=True,default=dict)
+    order_type = models.SmallIntegerField(choices=ORDER_TYPE_CHOICES_PVC,default=1)
+    created_at =models.DateTimeField(auto_now_add=True)
+    updated_at =models.DateTimeField(auto_now=True)
