b4e8839989f70ad00bca7dc9861726ed214b19cd
MuzaffarTursunov96 <muzaffar.tursunov102196@gmail.com>
2023-10-12 15:04:20 +0500

    order urls cprected

--- order/urls.py
+++ order/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('aluminiy',views.index,name='order'),
+    path('aluminiy-zayavki',views.index,name='order'),
     path('order-detail/<int:id>',views.order_detail,name='order_detail'),
     path('order-delete/<int:id>',views.order_delete,name='order_delete'),
     path('order-status/<int:id>',views.status_change_to_done,name ='status_change_to_done'),
