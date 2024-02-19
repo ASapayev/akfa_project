92ab2d41ac530c30ac939785734dfbfc8baf6242
MuzaffarTursunov96 <muzaffar.tursunov102196@gmail.com>
2024-02-19 11:24:39 +0500

    sdf

--- aluminiytermo/utils.py
+++ aluminiytermo/utils.py
@@ -3063,8 +3063,8 @@ def characteristika_created_txt_create_1301(datas):
 
         umumiy_without_duplicate1201[8].append(spart)
 
-        umumiy_without_duplicate1201[9].append(row['BRGEW'])
-        umumiy_without_duplicate1201[10].append(row['NTGEW'])
+        umumiy_without_duplicate1201[9].append(row['BRGEW'].replace('.',','))
+        umumiy_without_duplicate1201[10].append(row['NTGEW'].replace('.',','))
         umumiy_without_duplicate1201[11].append('')
         umumiy_without_duplicate1201[12].append('NORM')
         umumiy_without_duplicate1201[13].append(row['MAKTX'])
