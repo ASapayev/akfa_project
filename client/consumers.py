ba85c243cd5ace1d2470f7b1ff70d1a357ea08b8
MuzaffarTursunov96 <muzaffar.tursunov102196@gmail.com>
2024-04-30 17:07:47 +0500

    consumer corected

--- client/consumers.py
+++ client/consumers.py
@@ -4,49 +4,6 @@ from channels.layers import get_channel_layer
 from asgiref.sync import async_to_sync
 
 
-class ChatRoomConsumer(AsyncWebsocketConsumer):
-    async def connect(self):
-        print(self)
-        self.chat_box_name = self.scope["url_route"]["kwargs"]["chat_box_name"]
-        self.group_name = "chat_%s" % self.chat_box_name
-
-        await self.channel_layer.group_add(self.group_name, self.channel_name)
-
-        await self.accept()
-
-    async def disconnect(self, close_code):
-        print('asssssssaaaa')
-        await self.channel_layer.group_discard(self.group_name, self.channel_name)
-    # This function receive messages from WebSocket.
-    async def receive(self, text_data):
-        text_data_json = json.loads(text_data)
-        message = text_data_json["message"]
-        username = text_data_json["username"]
-
-        await self.channel_layer.group_send(
-            self.group_name,
-            {
-                "type": "chatbox_message",
-                "message": message,
-                "username": username,
-            },
-        )
-    # Receive message from room group.
-    async def chatbox_message(self, event):
-        message = event["message"]
-        username = event["username"]
-        #send message and username of sender to websocket
-        await self.send(
-            text_data=json.dumps(
-                {
-                    "message": message,
-                    "username": username,
-                }
-            )
-        )
-
-    pass
-
 class MessageConsumer(AsyncWebsocketConsumer):
     async def connect(self):
         print('connected>>>>>')
@@ -72,14 +29,14 @@ class MessageConsumer(AsyncWebsocketConsumer):
         text_data_json = json.loads(text_data)
         # print(text_data_json,'weebbbssss')
         chat_id = text_data_json['chat_id']
-        message_id = text_data_json['message_id']
-        user_id = text_data_json['user_id']
-        text = text_data_json['text']
-        owner = text_data_json['owner']
-        image = text_data_json['image']
-        username = text_data_json['username']
-        msg_type = text_data_json['msg_type']
-        file = text_data_json['file']
+        # message_id = text_data_json['message_id']
+        # user_id = text_data_json['user_id']
+        # text = text_data_json['text']
+        # owner = text_data_json['owner']
+        # image = text_data_json['image']
+        # username = text_data_json['username']
+        # msg_type = text_data_json['msg_type']
+        # file = text_data_json['file']
 
         # Process the message and prepare the response
         chanell = self.channel_layer
@@ -92,14 +49,14 @@ class MessageConsumer(AsyncWebsocketConsumer):
                 "client":['3.89.144.86','80'],
                 "type": "chatbox_message",
                 "chat_id": chat_id,
-                "message_id": message_id,
-                "user_id": user_id,
-                "text": text,
-                "owner": owner,
-                "image":image,
-                "username":username,
-                "msg_type":msg_type,
-                "file":file
+                # "message_id": message_id,
+                # "user_id": user_id,
+                # "text": text,
+                # "owner": owner,
+                # "image":image,
+                # "username":username,
+                # "msg_type":msg_type,
+                # "file":file
             },
         )
         print('after>>>>>> ',self.channel_layer,self.scope)
@@ -111,25 +68,25 @@ class MessageConsumer(AsyncWebsocketConsumer):
     async def chatbox_message(self, event):
         print('send>>>')
         chat_id = event["chat_id"]
-        message_id = event["message_id"]
-        user_id = event["user_id"]
-        text = event["text"]
-        owner = event["owner"]
-        image = event["image"]
-        username = event["username"]
-        msg_type = event["msg_type"]
-        file = event["file"]
+        # message_id = event["message_id"]
+        # user_id = event["user_id"]
+        # text = event["text"]
+        # owner = event["owner"]
+        # image = event["image"]
+        # username = event["username"]
+        # msg_type = event["msg_type"]
+        # file = event["file"]
 
         message_data = {
             "chat_id": chat_id,
-            "message_id": message_id,
-            "user_id": user_id,
-            "text": text,
-            "owner": owner,
-            "image": image,
-            "username": username,
-            "msg_type": msg_type,
-            "file": file
+            # "message_id": message_id,
+            # "user_id": user_id,
+            # "text": text,
+            # "owner": owner,
+            # "image": image,
+            # "username": username,
+            # "msg_type": msg_type,
+            # "file": file
         }
         await self.send(text_data=json.dumps(message_data))
         
--- config/asgi.py
+++ config/asgi.py
@@ -13,7 +13,7 @@ from django.core.asgi import get_asgi_application
 
 from channels.routing import ProtocolTypeRouter,URLRouter
 from channels.auth import AuthMiddlewareStack
-import aluminiytermo.routing 
+import client.routing
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'config.settings')
 
@@ -23,7 +23,7 @@ application = ProtocolTypeRouter({
     'http':get_asgi_application(),
     'websocket':AuthMiddlewareStack(
         URLRouter(
-            aluminiytermo.routing.websocket_urlpatterns
+            client.routing.websocket_urlpatterns
         )
     )
 })
