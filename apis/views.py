8068913f35cadd06768f0c5bc222e41dfed2cbd8
MuzaffarTursunov96 <muzaffar.tursunov102196@gmail.com>
2024-07-16 11:36:59 +0500

    pagination removed

--- apis/views.py
+++ apis/views.py
@@ -4,23 +4,17 @@ from .serializers import ProfileSerializer
 from rest_framework import viewsets
 from rest_framework.response import Response
 from aluminiy.models import AluProfilesData
-from rest_framework.pagination import PageNumberPagination
-
-class CustomPagination(PageNumberPagination):
-    page_size = 50  # Number of items per page
-    page_size_query_param = 'page_size'  # Override default page size query parameter name
-    max_page_size = 1000 
 
 
-class ProfileViewSet(viewsets.ModelViewSet):
+class ProfileViewSet(viewsets.ViewSet):
     """
     A simple ViewSet for listing or retrieving users.
     """
-    queryset = AluProfilesData.objects.all().order_by('created_at')
-    serializer_class = ProfileSerializer
-    pagination_class = CustomPagination
-
-
+    
+    def list(self, request):
+        queryset = AluProfilesData.objects.all()
+        serializer = ProfileSerializer(queryset, many=True)
+        return Response(serializer.data)
 
     # def retrieve(self, request, pk=None):
     #     queryset = User.objects.all()
