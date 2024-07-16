7855e983b650aec95c59b283ad4c16bfd555f931
MuzaffarTursunov96 <muzaffar.tursunov102196@gmail.com>
2024-07-16 11:27:23 +0500

    corected

--- config/settings.py
+++ config/settings.py
@@ -98,7 +98,10 @@ CHANNEL_LAYERS ={
     }
 }
 
-
+REST_FRAMEWORK = {
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 10
+}
 
 WSGI_APPLICATION = 'config.wsgi.application'
 
